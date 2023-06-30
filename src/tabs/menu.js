1117ee215f33036f3b76c690234441ef151b1dcf
18praise <jesussosarollie1812@gmail.com>
2023-06-29 22:49:41 -0300

    style.

--- src/tabs/menu.js
+++ src/tabs/menu.js
@@ -1,11 +1,21 @@
 import erase from "../functions/erase";
+import menu1 from "../images/menu1.jpg";
+import menu2 from "../images/menu2.jpg";
+import menu3 from "../images/menu3.jpg";
 
 export default function loadMenu() {
   erase();
   const content = document.getElementById("content");
+  const menuContainer = document.createElement("div");
+  menuContainer.classList.add("menuContainer");
+  const menuImages = [menu1, menu2, menu3];
 
-  const p = document.createElement("p");
-  p.innerText = "menu";
+  menuImages.forEach((menu) => {
+    const myImg = new Image();
+    myImg.src = menu;
+    myImg.classList.add("menuImage");
+    menuContainer.appendChild(myImg);
+  });
 
-  content.appendChild(p);
+  content.appendChild(menuContainer);
 }
